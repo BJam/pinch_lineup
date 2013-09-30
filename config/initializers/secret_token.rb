ef1dd23fbafbd9b97c9a61f8b97a43e49677e824
Brian Jameson <bjameson@sparkbase.com>
2013-09-29 23:49:27 -0400

    Initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PinchLineup::Application.config.secret_key_base = '57561a71a3b09376f3b024323319fd2b74ebeeb89212a9844e302e1a03f496340328ad20aef3cc31a1290eb276362600c141ddd9e5afdae47acccff00ea9baa9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PinchLineup::Application.config.session_store :cookie_store, key: '_pinch_lineup_session'
